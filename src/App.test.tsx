859bc79b4a95851d07f89453f9c654c0ecddac00
Anton Serdyuchenko <anton415@gmail.com>
2022-04-12 10:37:54 +0300

    First test and empty class.
    - Class: Simple, empty class for first test.
    - Test: When we didn't knock out any pins, then we have 0 score.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import {Bowling} from './Bowling';
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+describe("Bowling", () => {
+    // when we didn't knock out any pins, after 20 attempts
+    // then we have 0 score.
+    it("didn't knock out any pins", () => {
+        let bowling: Bowling = new Bowling();
+        for (let i = 0; i < 20; i++) {
+            bowling.addThrow(0);
+        }
+        // After 20 throws, we have 0 score.
+        expect(bowling.getScore()).toBe(0);
+    });
 });
---
+++ src/Bowling.ts
@@ -0,0 +1,11 @@
+export class Bowling {
+    // Add throw.
+    // pins - number of knock out pins.
+    public addThrow(pins: number): void {}
+
+    // Get score.
+    // return score number.
+    public getScore(): number {
+        return 0;
+    }
+}
