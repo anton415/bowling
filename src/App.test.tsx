c879fd6ef9923a1d58c8c5d51ce33d652a198695
Anton Serdyuchenko <anton415@gmail.com>
2022-04-12 11:18:06 +0300

    More tests and score calculation.
    - Class: Score calculation.
    - Test: More tests + refactoring.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -10,14 +10,44 @@ test('renders learn react link', () => {
 });
 
 describe("Bowling", () => {
+    let bowling: Bowling;
+
+    // Create new Bowling object before each test.
+    beforeEach(() => {
+        bowling = new Bowling();
+    });
+
+    // Add throws function.
+    // pins - number of pins we knock out.
+    // times - number of times throw will be repeated.
+    let addThrows = (pins: number, times: number) => {
+        for (let i = 0; i < times; i++) {
+            bowling.addThrow(pins);
+        }
+    };
+
     // when we didn't knock out any pins, after 20 attempts
     // then we have 0 score.
     it("didn't knock out any pins", () => {
-        let bowling: Bowling = new Bowling();
-        for (let i = 0; i < 20; i++) {
-            bowling.addThrow(0);
-        }
+        addThrows(0, 20);
         // After 20 throws, we have 0 score.
         expect(bowling.getScore()).toBe(0);
+    });
+
+    // when we knock out three pins, in every throws
+    // then we have 60 score.
+    it("three pins, in every throws", () => {
+        addThrows(3, 20);
+        // After 20 throws, we have 60 score.
+        expect(bowling.getScore()).toBe(60);
+    });
+
+    // when we knock out spare
+    // then we have 10 score.
+    it("spare", () => {
+        addThrows(5, 2);
+        addThrows(0, 18);
+        // After spare, we have 10 score.
+        expect(bowling.getScore()).toBe(10);
     });
 });
--- src/Bowling.ts
+++ src/Bowling.ts
@@ -1,11 +1,16 @@
 export class Bowling {
+    // Score number.
+    private score: number = 0;
+
     // Add throw.
     // pins - number of knock out pins.
-    public addThrow(pins: number): void {}
+    public addThrow(pins: number): void {
+        this.score += pins;
+    }
 
     // Get score.
     // return score number.
     public getScore(): number {
-        return 0;
+        return this.score;
     }
 }
