f896d220675e14113bfaa6c42c0d3188ab7fd94e
Anton Serdyuchenko <anton415@gmail.com>
2022-05-09 22:06:02 +0300

    Refactoring table view.

--- src/view/BowlingTable.tsx
+++ src/view/BowlingTable.tsx
@@ -21,63 +21,28 @@ function getFrameScore(frame: Frame): string {
 }
 
 export default class BowlingTable extends Component<Props> {
-
   render () {
     return (
       <Table>
         <TableBody>
           <TableRow>
-            <TableCell>{getThrow(this.props.frames[0], 0)}</TableCell>
-            <TableCell>{getThrow(this.props.frames[0], 1)}</TableCell>
-            <TableCell>{getThrow(this.props.frames[1], 0)}</TableCell>
-            <TableCell>{getThrow(this.props.frames[1], 1)}</TableCell>
-            <TableCell>{getThrow(this.props.frames[2], 0)}</TableCell>
-            <TableCell>{getThrow(this.props.frames[2], 1)}</TableCell>
-            <TableCell>{getThrow(this.props.frames[3], 0)}</TableCell>
-            <TableCell>{getThrow(this.props.frames[3], 1)}</TableCell>
-            <TableCell>{getThrow(this.props.frames[4], 0)}</TableCell>
-            <TableCell>{getThrow(this.props.frames[4], 1)}</TableCell>
-            <TableCell>{getThrow(this.props.frames[5], 0)}</TableCell>
-            <TableCell>{getThrow(this.props.frames[5], 1)}</TableCell>
-            <TableCell>{getThrow(this.props.frames[6], 0)}</TableCell>
-            <TableCell>{getThrow(this.props.frames[6], 1)}</TableCell>
-            <TableCell>{getThrow(this.props.frames[7], 0)}</TableCell>
-            <TableCell>{getThrow(this.props.frames[7], 1)}</TableCell>
-            <TableCell>{getThrow(this.props.frames[8], 0)}</TableCell>
-            <TableCell>{getThrow(this.props.frames[8], 1)}</TableCell>
-            <TableCell>{getThrow(this.props.frames[9], 0)}</TableCell>
-            <TableCell>{getThrow(this.props.frames[9], 1)}</TableCell>
-            <TableCell>{getThrow(this.props.frames[10], 0)}</TableCell>
-            <TableCell></TableCell>
+            {this.props.frames.map((frame) =>
+              <>
+                <TableCell>{getThrow(frame, 0)}</TableCell>
+                <TableCell>{getThrow(frame, 1)}</TableCell>
+              </>
+            )}
           </TableRow>
           <TableRow>
-            <TableCell>{getFrameScore(this.props.frames[0])}</TableCell>
-            <TableCell></TableCell>
-            <TableCell>{getFrameScore(this.props.frames[1])}</TableCell>
-            <TableCell></TableCell>
-            <TableCell>{getFrameScore(this.props.frames[2])}</TableCell>
-            <TableCell></TableCell>
-            <TableCell>{getFrameScore(this.props.frames[3])}</TableCell>
-            <TableCell></TableCell>
-            <TableCell>{getFrameScore(this.props.frames[4])}</TableCell>
-            <TableCell></TableCell>
-            <TableCell>{getFrameScore(this.props.frames[5])}</TableCell>
-            <TableCell></TableCell>
-            <TableCell>{getFrameScore(this.props.frames[6])}</TableCell>
-            <TableCell></TableCell>
-            <TableCell>{getFrameScore(this.props.frames[7])}</TableCell>
-            <TableCell></TableCell>
-            <TableCell>{getFrameScore(this.props.frames[8])}</TableCell>
-            <TableCell></TableCell>
-            <TableCell>{getFrameScore(this.props.frames[9])}</TableCell>
-            <TableCell></TableCell>
-            <TableCell>{getFrameScore(this.props.frames[10])}</TableCell>
-            <TableCell></TableCell>
+            {this.props.frames.map((frame) =>
+              <>
+                <TableCell>{getFrameScore(frame)}</TableCell>
+                <TableCell></TableCell>
+              </>
+            )}
           </TableRow>
         </TableBody>
       </Table>
-
-
     )
   }
 }
